e5eeb0f8b3332a9bf67782f8b9524943784caf11
Cameron Howell <jango.djarin@proton.me>
2025-09-21 23:40:15 -0500

    Fixed unit test fixture and other clippy warnings

--- src/theme/vscode.rs
+++ src/theme/vscode.rs
@@ -161,14 +161,14 @@ pub fn parse_vscode_theme(file: &str) -> anyhow::Result<Theme> {
         foreground: vscode_theme
             .colors
             .iter()
-            .find(|(c, _)| **c == "editorLineNumber.foreground".to_string())
+            .find(|(c, _)| **c == "editorLineNumber.foreground")
             .map(|(_, hex)| {
                 parse_rgb(hex.as_str().expect("colors are formatted as a hex string")).unwrap()
             }),
         background: vscode_theme
             .colors
             .iter()
-            .find(|(c, _)| **c == "editorLineNumber.background".to_string())
+            .find(|(c, _)| **c == "editorLineNumber.background")
             .map(|(_, hex)| {
                 parse_rgb(hex.as_str().expect("colors are formatted as a hex string")).unwrap()
             }),
