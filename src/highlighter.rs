287f2600e57f09d3b8ff985650867a066c6fc330
Cameron Howell <jango.djarin@proton.me>
2025-09-26 00:07:53 -0500

    Updated tree-sitter dependencies

--- src/highlighter.rs
+++ src/highlighter.rs
@@ -1,5 +1,5 @@
-use tree_sitter::{Parser, Query, QueryCursor};
-use tree_sitter_rust::{HIGHLIGHT_QUERY, language};
+use tree_sitter::{Parser, Query, QueryCursor, StreamingIterator};
+use tree_sitter_rust::HIGHLIGHTS_QUERY;
 
 use crate::{editor::StyleInfo, theme::Theme};
 
@@ -12,9 +12,10 @@ pub struct Highlighter {
 impl Highlighter {
     pub fn new(theme: &Theme) -> anyhow::Result<Self> {
         let mut parser = Parser::new();
-        let language = language();
-        parser.set_language(language)?;
-        let query = Query::new(language, HIGHLIGHT_QUERY)?;
+        parser
+            .set_language(&tree_sitter_rust::LANGUAGE.into())
+            .expect("Error loading Rust grammar");
+        let query = Query::new(&tree_sitter_rust::LANGUAGE.into(), HIGHLIGHTS_QUERY)?;
         let theme = theme.clone();
 
         Ok(Self {
@@ -29,14 +30,17 @@ impl Highlighter {
 
         let mut colors = Vec::new();
         let mut cursor = QueryCursor::new();
-        let matches = cursor.matches(&self.query, tree.root_node(), code.as_bytes());
+        let mut matches = cursor.matches(&self.query, tree.root_node(), code.as_bytes());
 
-        for mat in matches {
+        while let Some(mat) = {
+            matches.advance();
+            matches.get()
+        } {
             for cap in mat.captures {
                 let node = cap.node;
                 let start = node.start_byte();
                 let end = node.end_byte();
-                let scope = self.query.capture_names()[cap.index as usize].as_str();
+                let scope = self.query.capture_names()[cap.index as usize];
                 let style = self.theme.get_style(scope);
 
                 if let Some(style) = style {
