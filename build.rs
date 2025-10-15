3b91c415ca81fe04212026ef478cd0d65103d7c3
Cameron Howell <jango.djarin@proton.me>
2025-10-15 00:20:58 -0500

    Fixed clippy alerts

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+use std::borrow::Cow;
 use std::process::Command;
 
 const MAJOR: &str = env!("CARGO_PKG_VERSION_MAJOR");
@@ -6,14 +7,19 @@ const PATCH: &str = env!("CARGO_PKG_VERSION_PATCH");
 
 fn main() {
     let git_hash = Command::new("git")
-        .args(["rev-parse", "--short", "HEAD"])
+        .args(["rev-parse", "HEAD"])
         .output()
-        .map(|o| String::from_utf8_lossy(&o.stdout).trim().to_string())
-        .unwrap_or_else(|_| "unknown".into());
+        .ok()
+        .filter(|o| o.status.success())
+        .and_then(|x| String::from_utf8(x.stdout).ok())
+        .or_else(|| option_env!("").map(std::string::ToString::to_string));
 
     let ver_string = format!("{MAJOR}.{MINOR}.{PATCH}");
 
-    let version = format!("{} ({})", ver_string, git_hash);
+    let version: Cow<_> = match &git_hash {
+        Some(git_hash) => format!("{ver_string} ({})", &git_hash[..8]).into(),
+        None => ver_string.into(),
+    };
 
-    println!("cargo:rustc-env=VERSION_AND_GIT_HASH={}", version);
+    println!("cargo:rustc-env=VERSION_AND_GIT_HASH={version}");
 }
